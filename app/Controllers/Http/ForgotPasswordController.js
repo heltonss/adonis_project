45b5959fac8256092fc435c3f61c953726978ec4
Heltonss <Helton@heltonsouza.com.br>
2019-07-09 17:09:49 -0300

    add upload image --bug

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,28 +1,67 @@
-'use strict'
+'use strict';
 const cripto = require('crypto');
+const moment = require('moment');
 const User = use('App/Models/User');
-const Mail = use('Mail')
+const Mail = use('Mail');
 
 class ForgotPasswordController {
   async store({ request, response }) {
     try {
       const email = request.input('email');
       const user = await User.findByOrFail('email', email);
+      console.log(email);
 
       user.token = cripto.randomBytes(10).toString('hex');
       user.token_created_at = new Date();
-
       await user.save();
 
-      await Mail.send(['emails.forgot_password'], { email, token: user.token, link: `${request.input('redirect_url')}?token=4${uers.token}` }, message => {
-        message.to(user.email)
-          .from('helton@heltonsouza.com.br', 'Helton | T?')
-          .subject('Recuperação de senha')
-      })
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('helton@heltonsouza.com.br', 'Helton | T?')
+            .subject('Recuperação de senha');
+        }
+      );
     } catch (err) {
-      return response.status(err.status).send({ error: { message: 'esse email nao existe' } })
+      return response
+        .status(err.status)
+        .send({ error: { message: 'esse email nao existe' } });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at);
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'token expired' } });
+      }
+
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+
+      await user.save();
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'erro ao resetar senha' } });
     }
   }
 }
 
-module.exports = ForgotPasswordController
+module.exports = ForgotPasswordController;
--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +14,10 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
-Route.post('passwords', 'ForgotPasswordController.store')
+Route.post('users', 'UserController.store');
+Route.post('sessions', 'SessionController.store');
+Route.post('passwords', 'ForgotPasswordController.store');
+Route.put('passwords', 'ForgotPasswordController.update');
+Route.post('files', 'FileController.store');
