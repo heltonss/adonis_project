3d299f770d2a677187cdaedf2a47749a55c41dc0
heltonss <helton.silveira@grupoltm.com.br>
2019-07-10 18:05:42 -0300

    add - upload images

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -2,6 +2,7 @@
 
 class SessionController {
   async store({ request, response, auth }) {
+    console.log("teste")
     const { email, password } = request.all();
 
     const token = await auth.attempt(email, password);
