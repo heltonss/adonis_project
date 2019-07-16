bac7877c6bc5744711502e3c7f7e75253f799ed5
heltonss <helton.silveira@grupoltm.com.br>
2019-07-16 17:40:16 -0300

    add - transaction

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,10 +1,19 @@
 'use strict'
 const User = use('App/Models/User')
+const Database = use('Database')
 
 class UserController {
   async store({ request }) {
     const data = request.only(['username', 'email', 'password']);
+    const addresses = request.input('addresses')
+
+    const trx = await Database.beginTransaction();
+
     const user = await User.create(data);
+    await user.addresses().createMany(addresses)
+
+    await trx.commit();
+
     return user;
   }
 }
